9027ee9109ec1b36067acc7e39e616dcc8a82bb4
Andrey Prikhodko <5ray69@ukr.net>
2024-02-25 00:20:02 +0200

    Базовый класс модели представления ViewModels

---
+++ ViewModels/Base/ViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19.ViewModels.Base
+{
+    //INotifyPropertyChanged следит за изменениями
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //средство для генерации события, чтобы все наследники смогли им воспользоваться
+        //[CallerMemberName] string PropertyName = null компилятор автоматически подставит в PropertyName имя метода
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null) 
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        //Метод Set будет обновлять значение свойства для которого определено поле, 
+        //в котором это свойство хранит свои данные.
+        //Задача этого метода разрешить кольцевые изменения свойств, которые могут возникать
+        // - чтоб небыло кольцевого зацикливания и переполнения стека
+        //ref T field это ссылка на поле свойства
+        //T value это новое значение, которое мы хотим установить
+        //PropertyName это будет имя свойства, которое мы передадим в OnPropertyChanged
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null) 
+        {
+            //если значение поля соответствует тому значению, на которое мы
+            //хотим изменить это поле, то мы ничего не делаем
+            if(Equals(field, value)) return false;
+            field = value; //если значение изменилось, то обновляем поле и генерируем OnPropertyChanged
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
