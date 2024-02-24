0ddcb6ef48c86a1abb864cf31595b4c5bc77c409
Andrey Prikhodko <5ray69@ukr.net>
2024-02-25 00:55:44 +0200

    Добавляем интерфейс IDisposable к классу ViewModel для примера реализации

--- ViewModels/Base/ViewModel.cs
+++ ViewModels/Base/ViewModel.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 namespace CV19.ViewModels.Base
 {
     //INotifyPropertyChanged следит за изменениями
-    internal abstract class ViewModel : INotifyPropertyChanged
+    internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -36,5 +36,25 @@ namespace CV19.ViewModels.Base
             OnPropertyChanged(PropertyName);
             return true;
         }
+
+        //это пример как надо этот метод вызывать
+        //~ViewModel()
+        //{
+        //    Dispose(false);
+        //}
+
+        public void Dispose() 
+        {
+            Dispose(true);
+        }
+
+        private bool _Disposed;
+        protected virtual void Dispose(bool Disposing) 
+        {
+            if(!Disposing || _Disposed) return;
+            _Disposed = true;
+            //Освобождение управляемых ресурсов
+        }
+
     }
 }
