49477e6e43d5329c7c601aa6814ba44daed96ab7
Andrey Prikhodko <5ray69@ukr.net>
2024-02-25 23:30:13 +0200

    Revert "Тестирование работы модели-представления ViewModels"
    This reverts commit eada30324ad53e43c60a20e5d5257e53346f2350.

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,4 @@
-﻿using CV19.ViewModels.Base;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,36 +6,7 @@ using System.Threading.Tasks;
 
 namespace CV19.ViewModels
 {
-    internal class MainWindowViewModel : ViewModel
+    internal class MainWindowViewModel
     {
-        ////Пример переопределения метода Dispose и освобождения ресурсов, которые модель захватит
-        //protected override void Dispose(bool Disposing){ base.Dispose(Disposing); }
-
-        //Проверяем на работоспособность созданием свойство и подцепляем на него
-        //какие-то элементы, чтобы они из него подцепили данные.
-        //Для проверки создаем свойство с заголовком окна и статус, который должен отображаться внизу
-        //в статус баре
-
-        #region Заголовок окна
-        private string _Title = "Анализ статистики CV19"; //поле для данных заголовка окна
-        //В каждом свойстве должен быть такой код
-        /// <summary>Заголовок окна</summary>
-        public string Title
-        {
-            get => _Title;
-            //set 
-            //{
-            //    if (Equals(_Title != value)) return;
-            //    _Title = value;
-            //    OnPropertyChanged();
-            //}
-
-            //    Set(ref _Title, value); //упрощаем, тк мы написали метод Set
-            set => Set(ref _Title, value);
-        }
-        #endregion
     }
-
-
-
 }
