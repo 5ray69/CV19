958c4011c4b2af618832c8f29563855cc0069410
Andrey Prikhodko <5ray69@ukr.net>
2024-02-27 23:03:54 +0200

    Главный класс команды для нашего приложеня

---
+++ Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CV19.Infrastructure.Commands.Base
+{
+    internal abstract class Command : ICommand
+    {
+        //событие происходит, когда метод CanExecute переходит
+        //из одного состояния в другое
+        public event EventHandler CanExecuteChanged 
+        {
+            //передаем управление этим событием системе wpf, реализовав два элемента события add и remove
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+            //таким образом управление событием передали классу CommandManager и wpf автоматически
+            //генерирует это событие у всех команд, когда что-то происходит
+        }
+
+        //если метод возвращает false, то команду выполнить нельзя и элемент,
+        //к которому привязана команда отключается автоматически
+        public abstract bool CanExecute(object parameter);
+        
+        //то что должно быть выполнено самой командой,
+        //это основная логика которую должна выполнять команда
+        public abstract void Execute(object parameter);
+
+        //сделали методы абстрактными и их реализацией займется наследник
+    }
+}
---
+++ Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,29 @@
+﻿using CV19.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19.Infrastructure.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        //readonly чтоб никто их поменять не мог
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        //Action<object> этот делегат получает параметр класса object
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        //_CanExecute?.Invoke(parameter) считаем, что может быть пустая ссылка и 
+        // ?? true и если нет этого делегата, то считаем, что команду можно выполнить в любом случае
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Execute(parameter);
+    }
+}
