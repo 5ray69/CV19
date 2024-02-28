7e916d1279d612af05e0ee169747da6464e8f303
Andrey Prikhodko <5ray69@ukr.net>
2024-02-28 17:18:24 +0200

    Добавляем в модуль представления Главного окна команду выхода из приложения

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,12 @@
-﻿using CV19.ViewModels.Base;
+﻿using CV19.Infrastructure.Commands;
+using CV19.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace CV19.ViewModels
 {
@@ -49,6 +52,32 @@ namespace CV19.ViewModels
         }
 
         #endregion
+
+
+        #region Команды
+        #region CloseApplicationCommand
+        //команда, которая позволит закрывать нашу программу
+        public ICommand CloseApplicationCommand { get; } //это сама команда
+
+        //этот метод выполняется в тот момент когда команда выполняется
+        private void OnCloseApplicationCommandExecuted(object p) //это метод определяющий команду
+        {
+            Application.Current.Shutdown();
+        }
+
+        // => true обозначает, что метод всегда возвращает true (return true)
+        // написали вметсо тела метода в скобках {}
+        private bool CanCloseApplicationCommandExecute(object p) => true; //это метод определяющий команду
+
+        #endregion
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            #region Команды
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+            #endregion
+        }
 
     }
 }
