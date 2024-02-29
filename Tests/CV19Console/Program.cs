e76d6501940cbc4ec7b693223643881db1c2ba92
Andrey Prikhodko <5ray69@ukr.net>
2024-02-29 22:27:15 +0200

    запрос к удаленному серверу для получения сырых данных

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CV19Console
 {
-    internal class Program
+    class Program
     {
+        //private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv";
+        //private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/01-01-2021.csv";
+        private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //класс отправляет http-запросы - можно скачивать содержимое сайтов
+            //WebClient client = new WebClient(); //старая версия класса
+            var client = new HttpClient();
+
+            var response = client.GetAsync(data_url).Result;
+            var csv_str = response.Content.ReadAsStringAsync().Result;
+
+            //Console.WriteLine(response);
+
+            Console.ReadLine();
         }
     }
 }
