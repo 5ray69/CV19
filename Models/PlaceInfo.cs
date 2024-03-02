29b1a8eb8020074f06ce3a8bf6ab6b102ad8b5ef
Andrey Prikhodko <5ray69@ukr.net>
2024-03-02 17:28:08 +0200

    Классы из Country вынесены в Model

---
+++ Models/PlaceInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CV19.Models
+{
+    internal class PlaceInfo
+    {
+        public string Name { get; set; }
+
+        //Point-существующая структура в языке C#
+        public Point Location { get; set; }
+
+        public IEnumerable<ConfirmedCount> Count { get; set; }
+    }
+}
