ce0f97f9da731b3cc527473169f81b49d9b086ad
Andrey Prikhodko <5ray69@ukr.net>
2024-03-02 19:53:25 +0200

    Промежуточные значения

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CV19.Infrastructure.Commands;
+using CV19.Models;
 using CV19.ViewModels.Base;
 using System;
 using System.Collections.Generic;
@@ -19,6 +20,19 @@ namespace CV19.ViewModels
         //какие-то элементы, чтобы они из него подцепили данные.
         //Для проверки создаем свойство с заголовком окна и статус, который должен отображаться внизу
         //в статус баре
+
+        #region TestDataPoints : IEnumerable<DataPoint> - Тестовый набор данных для визуализации графиков
+
+        /// <summary>Тестовый набор данных для визуализации графиков/// </summary>
+        private IEnumerable<DataPoint> _TestDataPoints;
+
+        /// <summary>Тестовый набор данных для визуализации графиков/// </summary>
+        private IEnumerable<DataPoint> TestDataPoints
+        {
+            get => _TestDataPoints;
+            set=> Set(ref _TestDataPoints, value);
+        }
+        #endregion
 
         #region Заголовок окна
         private string _Title = "Анализ статистики CV19"; //поле для данных заголовка окна
@@ -75,8 +89,20 @@ namespace CV19.ViewModels
         public MainWindowViewModel()
         {
             #region Команды
+            
             CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+
             #endregion
+
+            var data_points = new List<DataPoint>((int)(360 / 0.1));
+            for (var x = 0d; x <= 360; x += 0.1)
+            {
+                const double to_rad = Math.PI / 180;
+                var y = Math.Sin(x * to_rad);
+                data_points.Add(new DataPoint { XValue = x, YValue = y });
+            }
+
+            TestDataPoints = data_points;
         }
 
     }
